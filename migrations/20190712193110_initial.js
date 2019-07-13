c4aa30b34803d30112f4637b08511e6c69e38546
Rane <github@blasph.me>
2019-07-12 21:41:39 -0700

    initialize knex and create migrations

---
+++ migrations/20190712193110_initial.js
@@ -0,0 +1,52 @@
+
+exports.up = async function(knex) {
+  await knex.schema.createTable('users', tbl => {
+        tbl.increments('id');
+        tbl.string('email').unique();
+        tbl.string('firstName');
+        tbl.string('lastName');
+        tbl.date('birthday');
+        tbl.string('phone');
+        tbl.string('location');
+        tbl.string('catchPhrase');
+        tbl.string('gender');
+        tbl.string('sexualOrientation');
+        tbl.string('imageUrl');
+  });
+
+  await knex.schema.createTable('interests', tbl => {
+      tbl.increments('id');
+      tbl.string('name');
+  });
+
+  await knex.schema.createTable('user_interests', tbl => {
+      tbl.integer('user_id')
+          .unsigned()
+         .references('id')
+         .inTable('users')
+         .notNullable();
+     tbl.integer('interest_id')
+         .unsigned()
+         .references('id')
+         .inTable('interests')
+         .notNullable();
+  });
+
+  await knex.schema.createTable('auth', tbl => {
+      tbl.integer('code')
+          .unsigned()
+          .notNullable();
+      tbl.integer('user_id')
+          .unsigned()
+          .references('id')
+          .inTable('users')
+          .notNullable();
+  })
+};
+
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists('users');
+  await knex.schema.dropTableIfExists('interests');
+  await knex.schema.dropTableIfExists('user_interests');
+  await knex.schema.dropTableIfExists('auth');
+};
--- server.js
+++ server.js
@@ -2,12 +2,34 @@ const express = require('express');
 const logger = require('morgan');
 const helmet = require('helmet');
 
+const { Pool, Client } = require('pg');
+
 const server = express();
 
 server.use(logger('dev'));
 server.use(helmet());
 
-server.get('/', (req, res) => {
+const keys = require('./keys/keys');
+
+const pgConfig = {
+    user: keys.pgUserName,
+    host: keys.pgHost,
+    database: keys.pgDBName,
+    password: keys.pgUserPW,
+    port: keys.pgDBPort
+};
+
+const pool = new Pool({ ...pgConfig });
+
+server.get('/', async (req, res) => {
+    // await pool.connect((err, client, release) => {
+    //     if (err) {
+    //         console.log(err);
+    //     } else {
+    //         return console.log("no error");
+    //     }
+    // });
+
     return res.status(200).send('<h1>Welcome to the server</h1>');
 });
 

60d519a5d320f6c58b2b131aac192a0425b9a399
Rane <github@blasph.me>
2019-07-16 08:57:49 -0700

    adding untracked files before merge

---
+++ keys/dev.js
@@ -0,0 +1,10 @@
+module.exports = {
+    pgHost: "friendfinder.crmhlp1jpi6y.us-east-2.rds.amazonaws.com",
+    pgUserPW: "lfsdahfd888fadsklf",
+    pgUserName: "friendfinder_user",
+    pgDBId: "friendfinder",
+    pgDBName: "friendfinderdb",
+    pgDBPort: 5432,
+    pgKMSKeyID: "f6871412-223b-45bd-af00-75654f672c75",
+    pgConnectionUrl: "postgres://friendfinder_user:lKY#w72B!D7@friendfinder.crmhlp1jpi6y.us-east-2.rds.amazonaws.com:5432/friendfinderdb"
+}

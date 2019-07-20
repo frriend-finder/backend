8dc7a07b7411c283f44a0c827ea8e57ed73cbdbc
Rane <github@blasph.me>
2019-07-19 20:16:16 -0700

    friends routes code cleanup, comments, and documentation

--- Friends/friendsModel.js
+++ Friends/friendsModel.js
@@ -1,17 +1,34 @@
 const db = require('../helpers/dbHelper');
 
+/**
+ * Get all friends for the user with user id user_id
+ * @param user_id
+ * @returns an array of user ids for all friends
+ */
 const getFriends = async user_id => {
     const friends = await db('user_friends').pluck('friend_id').where({ user_id });
 
     return friends;
 }
 
+/**
+ * Add a friend to a user
+ * @param user_id
+ * @param friend_id
+ * @returns number of rows inserted
+ */
 const addFriend = async (user_id, friend_id) => {
     const result = await db('user_friends').insert({ user_id, friend_id});
 
     return result;
 }
 
+/**
+ * Remove a friend from a user
+ * @param user_id
+ * @param friend_id
+ * @returns number of rows deleted
+ */
 const removeFriend = async (user_id, friend_id) => {
     const result = await db('user_friends').where({ user_id, friend_id }).delete();
 
